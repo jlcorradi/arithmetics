8266c6f6d03a809cdcb0a8925b973cc24b0b273c
Jorge Corradi <jlcorradi@gmail.com>
2023-08-16 22:17:58 -0300

    [FIX] Finishing Authentication

--- src/main/java/dev/jlcorradi/Arithmetics/web/HttpUtils.java
+++ src/main/java/dev/jlcorradi/Arithmetics/web/HttpUtils.java
@@ -1,6 +1,5 @@
 package dev.jlcorradi.Arithmetics.web;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -8,7 +7,6 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.Objects;
-import java.util.Optional;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class HttpUtils {
--- src/test/java/dev/jlcorradi/Arithmetics/ApplicationTests.java
+++ src/test/java/dev/jlcorradi/Arithmetics/ApplicationTests.java
@@ -3,15 +3,14 @@ package dev.jlcorradi.Arithmetics;
 import dev.jlcorradi.Arithmetics.web.security.TestContextSecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
 @ContextConfiguration(classes = TestContextSecurityConfig.class)
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
