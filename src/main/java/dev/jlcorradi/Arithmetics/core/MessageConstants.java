fb98feea00b0abf1ea8c2f66b4aa8bd09d0025ed
Jorge Corradi <jlcorradi@gmail.com>
2023-08-18 14:17:24 -0300

    [FEAT] Minor Tweaks

--- src/main/java/dev/jlcorradi/Arithmetics/core/MessageConstants.java
+++ src/main/java/dev/jlcorradi/Arithmetics/core/MessageConstants.java
@@ -10,7 +10,7 @@ public class MessageConstants {
   public static final String RESOURCE_NOT_FOUND_ERR = "Resource not found";
   public static final String WRONG_INPUT_COUNT_ERR = "Wrong number of parameters";
   public static final String UNSUPPORTED_OPERATION_ERR = "Unsupported Operation";
-  public static final String DIVISION_BY_ZERO_ERR = "Division By Zero";
+  public static final String DIVISION_BY_ZERO_ERR = "Division By Zero not allowed";
   public static final String RETRIEVING_RANDOM_STRING_ERR = "Error Retrieving Random String";
   public static final String INVALID_LENGTH_ERR = "Invalid Length";
   public static final String GENERIC_EXECUTION_ERR = "An error occurred while trying to execute the operation. Make sure the parameters are correct";
