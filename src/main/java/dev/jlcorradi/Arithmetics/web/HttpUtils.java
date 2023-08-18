646648ac84ec220ab200f3c6e9086dbe6fb425a5
Jorge Corradi <jlcorradi@gmail.com>
2023-08-18 00:13:50 -0300

    [FEAT] Readme

--- src/main/java/dev/jlcorradi/Arithmetics/web/HttpUtils.java
+++ src/main/java/dev/jlcorradi/Arithmetics/web/HttpUtils.java
@@ -11,9 +11,18 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import java.util.Objects;
 import java.util.Optional;
 
+/**
+ * Utility class for working with HTTP-related tasks.
+ */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class HttpUtils {
 
+  /**
+   * Gets the current HttpServletResponse associated with the request.
+   *
+   * @return The current HttpServletResponse instance.
+   * @throws IllegalStateException If there is no active request.
+   */
   public static HttpServletResponse getHttpServletResponse() {
     HttpServletResponse response =
         ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes()))
@@ -22,13 +31,26 @@ public class HttpUtils {
     return response;
   }
 
+  /**
+   * Adds a header message to the HttpServletResponse.
+   *
+   * @param messageType The type of the header message.
+   * @param message     The message content to be added.
+   */
   public static void addHeaderMessage(HeaderMessageType messageType, String message) {
     HttpServletResponse response = HttpUtils.getHttpServletResponse();
     response.addHeader(messageType.getHeaderName(), message);
   }
 
+  /**
+   * Retrieves the currently logged-in user details from the SecurityContextHolder.
+   *
+   * @return An Optional containing the logged-in user's details if available,
+   * or an empty Optional if not logged in or details are not available.
+   */
   public static Optional<ArithmeticsUser> getLoggedinUser() {
     return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication().getDetails())
         .map(o -> (ArithmeticsUser) o);
   }
 }
+
