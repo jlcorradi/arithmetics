43dfe5b5bc28f7ce6e7984838d011718420ce5a2
Jorge Corradi <jlcorradi@gmail.com>
2023-08-17 23:01:13 -0300

    [FEAT] Finishing Operations

--- src/main/java/dev/jlcorradi/Arithmetics/core/webclient/randomdotnet/RandomOrgClient.java
+++ src/main/java/dev/jlcorradi/Arithmetics/core/webclient/randomdotnet/RandomOrgClient.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name = "randomOrgClient", url = "${randomDotOrg.apiUrl}")
 public interface RandomOrgClient {
-  // TODO: Add Hystrix support
-
   @RequestMapping(method = RequestMethod.POST, value = "/json-rpc/2/invoke", consumes = "application/json")
   RandomDotNetGenerateRandomStringResponse generateRandomString(@RequestBody RandomDotNetGenerateRandomStringRequest request);
 }
